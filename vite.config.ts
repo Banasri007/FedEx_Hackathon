2a99fe4dec9d84bf8b041ccee7ec2dba233421b5
Banasri007 <unoun192114@gmail.com>
2026-01-10 00:32:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: Number(process.env.PORT) || 4173
-  },
-  server: {
-    port: 5173
+    port: Number(process.env.PORT),
+    host: "0.0.0.0"
   }
 });
